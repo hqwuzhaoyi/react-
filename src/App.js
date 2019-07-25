56dd803387ab407de33232728bfc03f06b966f7b
shawn wu <hqwuzhaoyi@qq.com>
2019-07-25 17:24:23 +0800

    post complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
-
-function App() {
-	return (
-		<div className="App" />
-	);
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/login" component={Login} />
+					<Route path="/posts" component={Home} />
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+// import PostList from './PostList';
 
-import PostList from './PostList';
-
-ReactDOM.render(<PostList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
